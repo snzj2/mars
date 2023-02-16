ced1a81562f0937af0ef027a3e98e810c8e9b68d
123 <baimuhametuv@gmail.com>
2023-02-16 14:15:35 +0500

    Личная карточка, сделал фон

--- mars_2/loginform.py
+++ mars_2/loginform.py
@@ -8,5 +8,7 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
+    username2 = StringField('Логин', validators=[DataRequired()])
+    password2 = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
+
--- mars_2/server.py
+++ mars_2/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, url_for, render_template, redirect, request
+from random import choice
 from loginform import *
+import json
+
 
 app = Flask(__name__)
 
@@ -11,19 +14,33 @@ def index(prof):
     return render_template('index.html', title='Заготовка', name=prof)
 
 
+@app.route('/member')
+def member():
+    with open('templates/crew.json', encoding='utf-8') as cat_file:
+        data = json.load(cat_file)
+    k = choice(data)
+
+    return render_template('member.html', name=k["name"], photo=k["photo"], work=", ".join(sorted(k["work"])))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     firstform = LoginForm()
-    secondform = LoginForm()
-    if firstform.validate_on_submit() and secondform.validate_on_submit():
-        print(secondform.validate_on_submit())
+    if firstform.validate_on_submit():
+        print(request.form)
+        print(request.form["username"])
+        print(request.form["password"])
+        print(request.form["username2"])
+        print(request.form["password2"])
         return redirect('/')
-    return render_template('login.html', title='Авторизация', form=firstform, form2=secondform)
+    return render_template('login.html', title='Авторизация', form=firstform)
 
 
 @app.route('/distribution')
 def distribution():
-    return render_template('distribution.html')
+    user_list = ['Руслан', 'Ридли скотт', 'Энди Уир', 'Кирилл']
+    return render_template('distribution.html', user_list=user_list)
+
+
 @app.route('/')
 def base():
     return render_template('base.html', title='Заготовка')
